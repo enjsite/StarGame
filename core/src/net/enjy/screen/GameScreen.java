e9cc89e4c0e2064624701334bae2d803247a7ff7
Natalia Episheva <enjyweb@gmail.com>
2019-04-18 00:23:48 +0300

    homework 4 stars

--- core/src/net/enjy/screen/GameScreen.java
+++ core/src/net/enjy/screen/GameScreen.java
@@ -1,6 +1,71 @@
 package net.enjy.screen;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 import net.enjy.base.BaseScreen;
+import net.enjy.math.Rect;
+import net.enjy.sprite.Background;
+import net.enjy.sprite.Star;
 
 public class GameScreen extends BaseScreen {
+
+    private Texture bg;
+    private Background background;
+    private TextureAtlas atlas;
+    private Star starList[];
+
+    @Override
+    public void show() {
+        super.show();
+
+        bg = new Texture("textures/bg2.jpg");
+        background = new Background(new TextureRegion(bg));
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        starList = new Star[256];
+        for (int i = 0; i < starList.length; i++){
+            starList[i] = new Star(atlas);
+        }
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : starList){
+            star.resize(worldBounds);
+        }
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        draw();
+    }
+
+    private void update(float delta){
+        for (Star star : starList){
+            star.update(delta);
+        }
+    }
+
+    private void draw(){
+        batch.begin();
+        background.draw(batch);
+        for (Star star : starList){
+            star.draw(batch);
+        }
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+        atlas.dispose();
+    }
+
+
 }
